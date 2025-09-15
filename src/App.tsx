f3d3d247d6b72032fa3f4d23fd21eb67a327ee8f
Perajti Pasupat <perajit.p@gmail.com>
2025-09-16 05:02:19 +0700

    feat: fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <>
       <AppHeader className="font-[OpenSans]" />
-      <main className="font-[OpenSans] min-h-screen bg-[#f4f4f4]">
+      <main className="font-[OpenSans] bg-[#f4f4f4] flex-1">
         <Outlet />
       </main>
       <AppFooter className="font-[OpenSans]" />
--- src/components/_base/BaseCheckbox/BaseCheckbox.tsx
+++ src/components/_base/BaseCheckbox/BaseCheckbox.tsx
@@ -8,7 +8,7 @@ export type BaseCheckboxProps = CheckboxProps & {
 
 const BaseCheckbox = forwardRef<HTMLInputElement, BaseCheckboxProps>((props, ref) => {
   const { id, label, errorMessage, className = '', ...otherProps } = props;
-  const baseClassName = 'shrink-0 !bg-transparent checked:!bg-current !border-neutral-400 rounded-xs';
+  const baseClassName = 'shrink-0 !bg-white checked:!bg-current !border-neutral-400 rounded-xs';
   const classNameForError = '!bg-red-100 !border-red-400 focus:!ring-1 focus:!ring-red-500';
 
   return (
